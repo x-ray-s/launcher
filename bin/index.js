619de79d1589590e71d0d9c17a530907b424d110
x-ray <princesswar721@gmail.com>
2022-05-07 16:54:52 +0800

    ts 配置

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
-
+import { exec } from 'child_process'
+import { Command } from 'commander'
+import enquirer from 'enquirer'
 import { stat, copyFile } from 'fs/promises'
 import path from 'path'
 import { fileURLToPath } from 'url'
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const files = ['.editorconfig', '.prettierrc', '.gitignore', '.eslintrc.js']
@@ -20,25 +23,43 @@ async function copy(filename) {
     return false
 }
 
-async function render(filename) {
-    // 123123123
-    var a = 1
-    var b = 2
-    var c = 42
-    console.log(a, b);
-    console.log(c);
-    console.log(a);
-    console.log(c);
-    console.log(d);
-    
-    
-}
-
 ;(async function () {
+    const program = new Command()
+    program.option('-c --confirm')
+
+    program.parse(process.argv)
+    const options = program.opts()
+    console.log(options)
     const iteration = files.map((file) => {
         return copy(file)
     })
     await Promise.all(iteration)
     console.log('copy finished')
     console.log('all be done')
+    if (options.confirm) {
+        const prompt = new enquirer.Confirm({
+            name: 'question',
+            message: 'Want to add tsconfig?',
+        })
+
+        prompt
+            .run()
+            .then((answer) => {
+                console.log(answer)
+                if (answer) {
+                    exec(
+                        'yarn add typescript @tsconfig/node16 -D',
+                        (err, stdout, stderr) => {
+                            if (err) {
+                                console.error(err)
+                                return
+                            }
+                            console.log(stdout)
+                            console.error(stderr)
+                        }
+                    )
+                }
+            })
+            .catch(console.error)
+    }
 })()
